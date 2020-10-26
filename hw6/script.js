6ddeea4b202bb37983d0a885a88dcca8ba474a3e
Max Yu <maxwellyu@college.harvard.edu>
2020-10-25 22:40:08 -0400

    adding files

---
+++ hw6/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+
+    var APIKey = "2aff5b55526d1fcac628dae6381ebd85"
+    //skyscanner apikey
+    var queryURL = "" + APIKey
+
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        var date = $()
+        var temp = $()
+        var humidity = 
+
+    })
+}) */
